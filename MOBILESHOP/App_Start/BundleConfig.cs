812a3b642d8a8c546892a3df41bbf0dd2ce6a40b
dell <dell@DESKTOP-J5P3N95>
2018-08-12 17:01:13 +0500

    Add project files.

---
+++ MOBILESHOP/App_Start/BundleConfig.cs
@@ -0,0 +1,58 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MOBILESHOP
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/script").Include(
+                      "~/assets/script.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/owl.carousel").Include(
+                      "~/assets/owl-carousel/owl.carousel.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/slitslider").Include(
+                      "~/assets/slitslider/js/modernizr.custom.79639.js",
+                      "~/assets/slitslider/js/jquery.ba-cond.min.js",
+                      "~/assets/slitslider/js/jquery.slitslider.js"));
+
+
+            //----------------------- CSS ------------------------//
+
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/assets/style.css"
+                      /*"~/Content/site.css"*/));
+
+            bundles.Add(new StyleBundle("~/Content/owl-carousel-css").Include(
+                     "~/assets/owl-carousel/owl.carousel.css",
+                     "~/assets/owl-carousel/owl.theme.css"
+                     ));
+            bundles.Add(new StyleBundle("~/Content/slitslider-css").Include(
+                     "~/assets/slitslider/css/style.css",
+                     "~/assets/slitslider/css/custom.css"
+                     ));
+
+        }
+    }
+}
