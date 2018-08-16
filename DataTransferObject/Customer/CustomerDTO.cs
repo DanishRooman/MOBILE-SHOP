7e45f4b289996f44cec27dada7337b3249c49fac
Muhammad Salman <Muhammad Salman@DESKTOP-MP3B7LD>
2018-08-16 09:01:41 -0700

    changes in customer detail

---
+++ DataTransferObject/Customer/CustomerDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferObject.Customer
+{
+   public class CustomerDTO
+    {
+        [Required]
+        [Display(Name = "id")]
+        public int id { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Mobile Number")]
+        public string Mobile { get; set; }
+
+    }
+}
---
+++ MOBILESHOP/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using DataTransferObject.Customer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MOBILESHOP.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public object CustomerDTO { get; private set; }
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            CustomerDTO  dto = new CustomerDTO();
+            return View();
+        }
+        public ActionResult AddCustomer()
+        {
+            CustomerDTO dt = new CustomerDTO();
+
+            return View();
+        }
+    }
+
+  
+}
