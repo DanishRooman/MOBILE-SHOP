92c55706a5e0760fce4011f66f8bb026e3e0700a
Muhammad Salman <Muhammad Salman@DESKTOP-MP3B7LD>
2018-09-11 21:33:55 -0700

    complete changes in service

---
+++ DataTransferObject/Services/ServicesDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferObject.Services
+{
+   public class ServiceDTO
+    {
+        public int id { get; set; }
+        [Required]
+        [Display(Name="Service Name")]
+        public string serviceName { get; set; }
+        [Required]
+        [Display(Name ="Service Charges")]
+        public string serviceCharges { get; set; }
+
+    }
+
+}
